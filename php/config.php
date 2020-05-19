5373e1fa5c8c083baf5ad31063b6e6cb94eda378
stefandr99 <61435988+stefandr99@users.noreply.github.com>
2020-05-19 20:57:53 +0300

    f

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
 //Config file for db connection
 
-define('DB_SERVER', 'localhost:8111');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'meq');
